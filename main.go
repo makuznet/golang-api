4fddc67171b131c642866ed3eb67ac08da953a54
makuznet <makuznet@gmail.com>
2021-04-28 12:00:51 +0300

    getProduct function created

--- main.go
+++ main.go
@@ -35,12 +35,12 @@ func main() {
 		fmt.Printf("%s\n", name)
 	}
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/v1/products/", getProducts)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 	defer db.Close()
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!\n", r.URL.Path[1:])
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "{\"products\": [{\"id\": 2, \"title\": \"Mens Casual Premium Slim Fit T-Shirts\", \"price\": 22.3, \"description\": \"Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.\", \"category\": \"mens clothing\",\"image\": \"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg\"}]}")
 }
