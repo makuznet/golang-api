a8bf21683ef52cef7dc7b756e82ef31729ae58dd
makuznet <makuznet@gmail.com>
2021-04-29 17:44:13 +0300

    Get product info by its id works

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"path"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
@@ -43,107 +42,96 @@ func main() {
 
 	fmt.Println("Server started")
 
-	http.HandleFunc("/v1/products/", getProducts)
-	http.HandleFunc("/v1/products/:id/", getProduct)
-	http.HandleFunc("/v1/products/add", addProduct)
+	// http.HandleFunc("/v1/products/", getProducts)
+	http.HandleFunc("/v1/products/", handler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// Get all products information
-func getProducts(w http.ResponseWriter, r *http.Request) {
+// handler function
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	productId, _ := strconv.Atoi(r.URL.Path[13:])
 
 	if r.Method != "GET" {
 		http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
+	} else if productId != 0 {
+
+		fmt.Fprintf(w, "query with userId:%d\n", productId)
+
+		rows, err := db.Query("SELECT id, title, price, description, category, image from products where id=$1", productId)
+		if err != nil {
+			panic(err)
+		}
+
+		for rows.Next() {
+			var id int
+			var title string
+			var price int
+			var description string
+			var category string
+			var image string
+
+			err = rows.Scan(&id, &title, &price, &description, &category, &image)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Fprintf(w, "%d\n %s\n %d\n %s\n %s\n %s\n", id, title, price, description, category, image)
+		}
+
 	} else {
-		w_array := Products{}
 
 		fmt.Println("# Querying")
-		rows, err := db.Query("SELECT id,title,price,description,category,image from products")
+		rows, err := db.Query("SELECT id, title, price, description, category, image from products")
 		if err != nil {
 			panic(err)
 		}
 
 		for rows.Next() {
-
-			w_product := Product{}
-
-			err = rows.Scan(&w_product.Id, &w_product.Title, &w_product.Price, &w_product.Description, &w_product.Category, &w_product.Image)
-			if err != nil {
-				panic(err)
-			}
-			w_array.Products = append(w_array.Products, w_product)
-		}
-
-		json.NewEncoder(w).Encode(w_array)
-	}
-}
-
-// Get some product info
-func getProduct(w http.ResponseWriter, r *http.Request) {
-
-	userId :=
-
-	if r.Method != "GET" {
-		http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
-	} else {
-		u_array := Products{}
-
-		// r.URL.Path[1:]
-
-		fmt.Println("# Eliborating")
-		rows, err := db.Query("SELECT id,title,price,description,category,image from products where id = $1", userId)
-		if err != nil {
-			panic(err)
-		}
-
-		for rows.Next() {
-
-			u_product := Product{}
-
-			err = rows.Scan(&u_product.Id, &u_product.Title, &u_product.Price, &u_product.Description, &u_product.Category, &u_product.Image)
-			if err != nil {
-				panic(err)
-			}
-			u_array.Products = append(u_array.Products, u_product)
-		}
-
-		json.NewEncoder(w).Encode(u_array)
-	}
-}
-
-// Add new product
-func addProduct(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
-	} else {
-
-		decoder := json.NewDecoder(r.Body)
-
-		var g_product Product
-
-		err := decoder.Decode(&g_product)
-		if err != nil {
-			panic(err)
-		}
-
-		query := fmt.Sprintf("INSERT INTO products(title, price, description, category, image) VALUES('%s', %d, '%s', '%s', '%s') RETURNING id", g_product.Title, g_product.Price, g_product.Description, g_product.Category, g_product.Image)
-
-		fmt.Println("# Inserting")
-		rows, err := db.Query(query)
-		if err != nil {
-			panic(err)
-		}
-
-		for rows.Next() {
-
 			var id int
+			var title string
+			var price int
+			var description string
+			var category string
+			var image string
 
-			err = rows.Scan(&id)
+			err = rows.Scan(&id, &title, &price, &description, &category, &image)
 			if err != nil {
 				panic(err)
 			}
-			fmt.Fprintf(w, "{\"id\":%d}", id)
+
+			fmt.Fprintf(w, "%d\n %s\n %d\n %s\n %s\n %s\n", id, title, price, description, category, image)
 		}
+
 	}
 }
+
+// Get all products information
+// func getProducts(w http.ResponseWriter, r *http.Request) {
+
+// 	if r.Method != "GET" {
+// 		http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
+// 	} else {
+// 		w_array := Products{}
+
+// 		fmt.Println("# Querying")
+// 		rows, err := db.Query("SELECT id,title,price,description,category,image from products")
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		for rows.Next() {
+
+// 			w_product := Product{}
+
+// 			err = rows.Scan(&w_product.Id, &w_product.Title, &w_product.Price, &w_product.Description, &w_product.Category, &w_product.Image)
+// 			if err != nil {
+// 				panic(err)
+// 			}
+// 			w_array.Products = append(w_array.Products, w_product)
+// 		}
+
+// 		json.NewEncoder(w).Encode(w_array)
+// 	}
+// }
